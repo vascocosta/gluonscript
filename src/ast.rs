2659e97af108a90f343abbbbd43014681f56e333
Vasco Costa <vascomacosta@gmail.com>
2026-03-19 11:45:46 +0000

    Implement not equal operator for strings

--- src/ast.rs
+++ src/ast.rs
@@ -119,6 +119,9 @@ impl Expr {
                     (Value::Float(a), Value::Int(b), Operator::EqualEqual) => {
                         Value::Bool(a == b as f64)
                     }
+                    (Value::String(a), Value::String(b), Operator::EqualEqual) => {
+                        Value::Bool(a == b)
+                    }
 
                     (Value::Int(a), Value::Int(b), Operator::NotEqual) => Value::Bool(a != b),
                     (Value::Float(a), Value::Float(b), Operator::NotEqual) => Value::Bool(a != b),
@@ -128,6 +131,7 @@ impl Expr {
                     (Value::Float(a), Value::Int(b), Operator::NotEqual) => {
                         Value::Bool(a != b as f64)
                     }
+                    (Value::String(a), Value::String(b), Operator::NotEqual) => Value::Bool(a != b),
 
                     (Value::String(a), Value::String(b), Operator::Add) => {
                         Value::String(format!("{}{}", a, b))
